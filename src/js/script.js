cd2bd6fa3ca7e46f79e41b9b2781dda3ae8aa677
Alicja Latała <alicja.latala@gmail.com>
2021-03-29 00:02:53 +0200

    Fix adding book to favouriteBooks

--- src/js/script.js
+++ src/js/script.js
@@ -21,7 +21,7 @@
       // console.log('HTML', HTML);
     }
     console.log('bookCointainer', bookContainer);
-    return bookContainer;
+    // return bookContainer;
   };
 
   const favouriteBooks = [];
@@ -29,20 +29,27 @@
   const initActions = function(){
     const bookLinks = document.querySelectorAll(select.bookLink);
     console.log('bookLinks', bookLinks);
+
     for(let bookLink of bookLinks){
       console.log('bookLink', bookLink);
       bookLink.addEventListener('dblclick', function(event){
         event.preventDefault();
-        bookLink.classList.toggle('favorite');
         const bookId = bookLink.getAttribute('data-id');
         console.log('bookId', bookId);
-        favouriteBooks.push(bookId);
+
+        if (favouriteBooks.indexOf(bookId) == -1){
+          bookLink.classList.add('favorite');
+          favouriteBooks.push(bookId);
+        }else{
+          bookLink.classList.remove('favorite');
+          favouriteBooks.pop(bookId);
+        }
+        console.log('favouriteBooks', favouriteBooks);
       });
     }
   };
 
   render();
   initActions();
-  
-  console.log('favouriteBooks', favouriteBooks);
+
 }
